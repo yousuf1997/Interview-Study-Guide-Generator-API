587bf9a04573d9dbc9d7c6295f56fd28774d13ec
Mohamed Abdul <mohamedyousufmo@gmail.com>
2025-10-16 19:05:55 -0700

    added slides

--- interview-study-generator-api/src/main/java/com/mohamed/interview_study_generator_api/config/SecurityConfig.java
+++ interview-study-generator-api/src/main/java/com/mohamed/interview_study_generator_api/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
